c073c576e5e1b0d641c9443f2ca135597a5608f3
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2022-10-12 14:21:07 +0200

    istruzioni

---
+++ JS/script.js
@@ -0,0 +1,83 @@
+/*
+SNACK_1: Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+fare un check che controlli odd or even;
+
+se even print;
+
+se odd print odd +1;
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+SNACK_2: Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
+
+
+creare due array con nomi e cognomi;
+
+creare una funzione che scelga elemento random nell'arary e lo pushi in una casella result concatenando;
+
+*/
+
+
+/*
+
+SNACK_3: Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari.
+
+creare array numeri;
+
+fare in modo che si selezionino solo i numeri ad index odd;
+
+sommare tali numeri tra di loro;
+
+*/
+
+/**
+ 
+SNACK_4(BONUS): Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+
+
+creare due array con diff numeri di elementi all'interno;
+
+fare check lunghezza di entrambi;
+
+creare condizione che push elementi nell'array con meno elementi fino a giungere a parità;
+
+ */
